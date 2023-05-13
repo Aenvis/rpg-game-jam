11442bd8e3b309b2628b0a558b8664819a936501
Sodment <sodment@gmail.com>
2023-05-13 19:35:00 +0200

    XD

---
+++ Assets/Scripts/Stary/StaryAnimationController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class StaryAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    public float minWaitTime = 5f; // Minimum time to wait before changing idle animation
+    public float maxWaitTime = 15f; // Maximum time to wait before changing idle animation
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine("IdleAnimationRoutine");
+    }
+
+    IEnumerator IdleAnimationRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+            // Randomly select one of the special idle animations
+            int randomIdle = Random.Range(2, 4);
+
+            if (randomIdle == 2)
+            {
+                animator.SetTrigger("Drinking");
+            }
+            else if (randomIdle == 3)
+            {
+                animator.SetTrigger("Stomach");
+            }
+        }
+    }
+}
